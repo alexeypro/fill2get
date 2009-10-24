74955571f873cbfeb8af05ff4df7f1b776891205
Olexiy Prokhorenko <olexiy@o-macair.gateway.2wire.net>
2009-10-24 13:59:54 -0700

    More simple logic

--- app.rb
+++ app.rb
@@ -1,10 +1,43 @@
-require "rubygems"
-require "sinatra"
+require 'rubygems'
+require 'sinatra'
+
+class String
+  def ends_with?(str)
+    str = str.to_str
+    tail = self[-str.length, str.length]
+    tail == str      
+  end
+end
+
  
 get '/' do
   erb :index
 end
 
-post '/save-answers' do
+get '/save-answers' do
   erb :index
+end
+
+post '/save-answers' do
+  no_firstname = (params == nil || params[:firstname].nil? || params[:firstname].empty?)
+  no_lastname = (params == nil || params[:lastname].nil? || params[:lastname].empty?)
+  if no_firstname || no_lastname
+    @error_message = 'Please fill all fields!'
+    erb :index
+  else
+    erb :thank_you
+  end
+end
+
+get '/download' do
+  right_referer = request.referer.ends_with?('/save-answers')
+  if right_referer
+    attachment "file_to_download.png"
+  else
+    redirect '/'
+  end
+end
+
+post '/download' do
+  redirect '/'
 end
